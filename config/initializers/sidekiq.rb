d87a5be382340bdbf577f036424aaa2989d4f525
Nada <nadakarim@student.aast.edu>
2024-08-16 22:05:58 +0300

    updated the authentication and redis to work on docker

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -4,10 +4,10 @@ require "sidekiq"
 
 Sidekiq.configure_server do |config|
   # Configure Sidekiq server to use Redis
-  config.redis = { url: "redis://localhost:6379/1" }
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://redis:6379/1") }
 end
 
 Sidekiq.configure_client do |config|
   # Configure Sidekiq client to use Redis
-  config.redis = { url: "redis://localhost:6379/1" }
+  config.redis = { url: ENV.fetch("REDIS_URL", "redis://redis:6379/1") }
 end
--- lib/json_web_token.rb
+++ lib/json_web_token.rb
@@ -1,20 +1,19 @@
 class JsonWebToken
-  SECRET_KEY = Rails.application.credentials.secret_key_base
-  class << self
+    class << self
     def encode(payload, exp = 24.hours.from_now)
       payload[:exp] = exp.to_i
-      JWT.encode(payload, SECRET_KEY)
+      JWT.encode(payload,  ENV["JWT_SECRET_KEY"])
       # Rails.logger.debug("encoded token: #{ JWT.encode(payload, SECRET_KEY)}")
     end
 
     def decode(token)
       #  Rails.logger.debug("Decoding token: #{token}")
-      body = JWT.decode(token, SECRET_KEY)[0]
+      body = JWT.decode(token, ENV["JWT_SECRET_KEY"])[0]
       # Rails.logger.debug("Decoded body: #{body}")
       HashWithIndifferentAccess.new body
     rescue JWT::DecodeError => e
       # Rails.logger.debug("JWT Decode Error: #{e.message}")
       nil
     end
-  end
+    end
 end
